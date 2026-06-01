d57952bc38cbf8e9f4d7033e81c69bfc13d93967
Paulo Padilha <paulopadilha2302@gmail.com>
2026-06-01 17:15:09 -0300

    Fix profile image FileNotFoundError in production and track default.jpg

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
@@ -10,12 +11,16 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Profile"
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         
-        img = Image.open(self.image.path)  
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        # Check if the image file exists before trying to open it to prevent FileNotFoundError in production
+        if self.image and os.path.exists(self.image.path):
+            try:
+                img = Image.open(self.image.path)
+                if img.height > 300 or img.width > 300:
+                    output_size = (300, 300)
+                    img.thumbnail(output_size)
+                    img.save(self.image.path)
+            except Exception:
+                pass
