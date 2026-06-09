2768cb7f85f86c6a4b6ecc39ffb3dc69aba5a707
Paulo Padilha <paulopadilha2302@gmail.com>
2026-06-09 14:56:23 -0300

    fix: resolve profile picture loading issue on deploy by fallback to static and serving media

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path, include, re_path
 from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -48,3 +49,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
--- users/models.py
+++ users/models.py
@@ -5,11 +5,29 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+    image = models.ImageField(default='default.png', upload_to='profile_pics/')
     bio = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            # Fallback to static default if it is the default image or missing on disk
+            if os.path.basename(self.image.name).startswith('default'):
+                from django.templatetags.static import static
+                return static('blog/default.png')
+            try:
+                if not os.path.exists(self.image.path):
+                     from django.templatetags.static import static
+                     return static('blog/default.png')
+            except Exception:
+                 from django.templatetags.static import static
+                 return static('blog/default.png')
+            return self.image.url
+        from django.templatetags.static import static
+        return static('blog/default.png')
     
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
